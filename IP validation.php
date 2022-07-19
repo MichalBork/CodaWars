6378e30ef74dc14408b617135c6bd04d8a8fd24a
michalbork <michalbork3@gmail.com>
2022-07-19 19:51:22 +0200

    Commit

--- IP validation.php
+++ IP validation.php
@@ -14,14 +14,12 @@
 //Leading zeros (e.g. 01.02.03.04) are considered invalid
 //Inputs are guaranteed to be a single string
 function isValidIP(string $str): bool
-
 {
-
-    $checkNumeric = str_split($str);
-    foreach ($checkNumeric as $key => $value) {
-        $a = preg_match("/[^0-9\.]/",$value);
-        if ($a > 0 ) return false;
+    $a = preg_match("/[^0-9\.]/", $str);
+    if ($a > 0) {
+        return false;
     }
+
 
     $ipAddress = array_map('intval', explode('.', $str));
     // TODO
@@ -32,21 +30,15 @@ function isValidIP(string $str): bool
     if (count($ipAddress) !== 4) {
         return false;
     }
-    foreach($ipAddress as $key => $value){
-        if($value > 255 ){
-            return false;
-        }
-        if($value < 0 ){
+    foreach ($ipAddress as $key => $value) {
+        if ($value > 255 || $value < 0) {
             return false;
         }
     }
     return true;
-
-
 }
 
 
-
-print_r(var_dump(isvalidIP('238.46.26.2t')));
+print_r(var_dump(isvalidIP('238.46.26.262')));
 
 
