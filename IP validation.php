8f8f8dc41be52d775003d8fbb2ea63edbde2fd58
michalbork <michalbork3@gmail.com>
2022-07-19 17:55:14 +0200

    Commit

--- IP validation.php
+++ IP validation.php
@@ -1,33 +1,52 @@
 <?php
+//Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+//
+//Valid inputs examples:
+//Examples of valid inputs:
+//1.2.3.4
+//123.45.67.89
+//Invalid input examples:
+//1.2.3
+//1.2.3.4.5
+//123.456.78.90
+//123.045.067.089
+//Notes:
+//Leading zeros (e.g. 01.02.03.04) are considered invalid
+//Inputs are guaranteed to be a single string
 function isValidIP(string $str): bool
 
 {
 
     $checkNumeric = str_split($str);
     foreach ($checkNumeric as $key => $value) {
-        if (!is_numeric($value[$key])) {
-            return false;
-        }
-
-        $ipAddress = array_map('intval', explode('.', $str));
-        // TODO
-        var_dump($ipAddress);
-        if (array_key_exists(4, $ipAddress)) {
-            return false;
-        }
-        if (count($ipAddress) < 3) {
-            return false;
-        }
-        foreach($ipAddress as $key => $value){
-            if($value > 255 ){
-                return false;
-            }
-            if($value < 0 ){
-                return false;
-            }
-        }
-        return true;
-
-
+        $a = preg_match("/[^0-9\.]/",$value);
+        if ($a > 0 ) return false;
     }
+
+    $ipAddress = array_map('intval', explode('.', $str));
+    // TODO
+    var_dump($ipAddress);
+    if (array_key_exists(4, $ipAddress)) {
+        return false;
+    }
+    if (count($ipAddress) !== 4) {
+        return false;
+    }
+    foreach($ipAddress as $key => $value){
+        if($value > 255 ){
+            return false;
+        }
+        if($value < 0 ){
+            return false;
+        }
+    }
+    return true;
+
+
 }
+
+
+
+print_r(var_dump(isvalidIP('238.46.26.2t')));
+
+
